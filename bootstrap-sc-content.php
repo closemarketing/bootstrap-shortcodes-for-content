0a0e9d194cd240ceae926d017edaa4ac58d2c269
David Perez <david@gibam.es>
2015-02-06 14:29:39 +0100

    First Version

---
+++ bootstrap-sc-content.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Bootstrap Shortcodes for Content
+Plugin URI: https://www.closemarketing.es
+Description: Twitter Bootstrap 3 shortcodes plugin for Content
+Author: David Perez
+Author URI: http://twitter.com/closemarketing
+Version: 0.1
+License: GNU General Public License version 3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+
+Include functions */
+require_once( dirname(__FILE__) . '/includes/shortcode-functions.php'); // Main shortcode functions
