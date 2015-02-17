300e5820da687c537230f1df865e1970cb93dbd2
David Perez <david@closemarketing.es>
2015-02-18 00:37:06 +0100

    New shortcode [imagepostslider]

--- includes/shortcode-functions.php
+++ includes/shortcode-functions.php
@@ -66,3 +66,53 @@ if( !function_exists('btsc_gridbox_shortcode') ) {
 	}
 	add_shortcode( 'gridbox', 'btsc_gridbox_shortcode' );
 }
+
+/*
+ * Image Post Slider
+ * @since v1.0
+ */
+if( !function_exists('btsc_imagepostslider_shortcode') ) {
+	function btsc_imagepostslider_shortcode($atts, $content = null) {
+        
+        global $post;
+        $args = array(
+            'post_mime_type' => 'image',
+            'post_parent' => $post->ID,
+            'post_type' => 'attachment',
+            'suppress_filters' => 0
+        );
+
+        $attachments = get_children( $args );
+        $numattach = count($attachments);
+
+        if ( $attachments && $numattach == 1 ) {
+            $html = get_the_post_thumbnail('page-thumb', array('class' => 'page-thumb img-rounded'));
+        } elseif ($attachments) {
+            
+        $html = '<div class="carousel slide" id="myCarousel">';
+        $html .= '<div class="carousel-inner">';
+
+        $i = 0;
+        foreach ( $attachments as $attachment ) {
+
+            $html .= '<div class="item ';
+            if($i==0) $html .= ' active';
+            $html .= '">';
+            $html .= '<div class="bannerImage">';
+            $html .= get_the_post_thumbnail( $post->ID, 'page-thumb' );
+            $html .= '</div>';
+            $html .= '</div><!-- /Slide -->' ;
+            $i++;
+        }
+        $html .= '</div>';
+        $html .= '<div class="control-box">';
+        $html .= '<a data-slide="prev" href="#myCarousel" class="carousel-control left">‹</a>';
+        $html .= '<a data-slide="next" href="#myCarousel" class="carousel-control right">›</a>';
+        $html .= '</div><!-- /.control-box -->';
+        $html .= '</div>';
+            
+        return $html;
+        }
+
+	add_shortcode( 'imagepostslider', 'btsc_imagepostslider_shortcode' );
+}
