d0964c6743811e3dfab7141f2b0fb03f4297afcc
David Perez <david@gibam.es>
2015-04-21 08:42:56 +0200

    Gallery

--- includes/shortcode-functions.php
+++ includes/shortcode-functions.php
@@ -52,12 +52,12 @@ if( !function_exists('btsc_gridbox_shortcode') ) {
         
         if ( $postsgrid->have_posts() ) :
         while ( $postsgrid->have_posts() ) : $postsgrid->the_post(); 
-            $html .= '<div class="gridbox-container col-sm-'.$colw.' col-xs-12">';
+            $html .= '<div class="gridbox-container thumbnail col-sm-'.$colw.' col-xs-12">';
             $html .= '<div class="gridbox-thumbnail">';
             $html .= '<a href="'.get_the_permalink($postsgrid->post->ID).'">';
             $html .= get_the_post_thumbnail($postsgrid->post->ID, 'thumb-col-'.$att['col']);
             $html .= '</a>';
-            $html .= '<div class="captiongrid">';
+            $html .= '<div class="captiongrid caption-hover">';
             $html .= '<h2 class="titlegrid">';
             if(esc_attr($att['date'])) { 
                 $html .= '<span class="postdate">';
@@ -127,4 +127,285 @@ if( !function_exists('btsc_imagepostslider_shortcode') ) {
         }
     }
 	add_shortcode( 'imagepostslider', 'btsc_imagepostslider_shortcode' );
+}
+
+/*
+ * Gallery Shortcode
+ * @since v0.1
+ */
+// Custom filter function to modify default gallery shortcode output
+function btsc_shortcode_gallery( $output, $attr ) {
+
+	// Initialize
+	global $post, $wp_locale;
+
+	// Gallery instance counter
+	static $instance = 0;
+	$instance++;
+
+	// Validate the author's orderby attribute
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( ! $attr['orderby'] ) unset( $attr['orderby'] );
+	}
+
+	// Get attributes from shortcode
+	extract( shortcode_atts( array(
+		'order'      => 'ASC',
+		'orderby'    => 'menu_order ID',
+		'id'         => $post->ID,
+		'itemtag'    => 'dl',
+		'icontag'    => 'dt',
+		'captiontag' => 'dd',
+		'columns'    => 3,
+		'size'       => 'thumbnail',
+		'include'    => '',
+		'exclude'    => ''
+	), $attr ) );
+
+	// Initialize
+	$id = intval( $id );
+	$attachments = array();
+	if ( $order == 'RAND' ) $orderby = 'none';
+
+	if ( ! empty( $include ) ) {
+
+		// Include attribute is present
+		$include = preg_replace( '/[^0-9,]+/', '', $include );
+		$_attachments = get_posts( array( 'include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+
+		// Setup attachments array
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[ $val->ID ] = $_attachments[ $key ];
+		}
+
+	} else if ( ! empty( $exclude ) ) {
+
+		// Exclude attribute is present 
+		$exclude = preg_replace( '/[^0-9,]+/', '', $exclude );
+
+		// Setup attachments array
+		$attachments = get_children( array( 'post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+	} else {
+		// Setup attachments array
+		$attachments = get_children( array( 'post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby ) );
+	}
+
+	if ( empty( $attachments ) ) return '';
+
+	// Filter gallery differently for feeds
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment ) $output .= wp_get_attachment_link( $att_id, $size, true ) . "\n";
+		return $output;
+	}
+
+	// Filter tags and attributes
+	$itemtag = tag_escape( $itemtag );
+	$captiontag = tag_escape( $captiontag );
+	$columns = intval( $columns );
+	$itemwidth = $columns > 0 ? floor( 100 / $columns ) : 100;
+	$float = is_rtl() ? 'right' : 'left';
+	$selector = "gallery-{$instance}";
+
+	// Filter gallery CSS
+	$output = apply_filters( 'gallery_style', "
+		<style type='text/css'>
+			#{$selector} {
+				margin: auto;
+			}
+			#{$selector} .gallery-item {
+				float: {$float};
+				margin-top: 10px;
+				text-align: center;
+				width: {$itemwidth}%;
+			}
+			#{$selector} img {
+				border: 2px solid #cfcfcf;
+			}
+			#{$selector} .gallery-caption {
+				margin-left: 0;
+			}
+		</style>
+		<!-- see gallery_shortcode() in wp-includes/media.php -->
+		<div id='$selector' class='gallery galleryid-{$id}'>"
+	);
+
+	// Iterate through the attachments in this gallery instance
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+
+		// Attachment link
+		$link = isset( $attr['link'] ) && 'file' == $attr['link'] ? wp_get_attachment_link( $id, $size, false, false ) : wp_get_attachment_link( $id, $size, true, false ); 
+
+		// Start itemtag
+		$output .= "<{$itemtag} class='gallery-item'>";
+
+		// icontag
+		$output .= "
+		<{$icontag} class='gallery-icon'>
+			$link
+		</{$icontag}>";
+
+		if ( $captiontag && trim( $attachment->post_excerpt ) ) {
+
+			// captiontag
+			$output .= "
+			<{$captiontag} class='gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+			</{$captiontag}>";
+
+		}
+
+		// End itemtag
+		$output .= "</{$itemtag}>";
+
+		// Line breaks by columns set
+		if($columns > 0 && ++$i % $columns == 0) $output .= '<br style="clear: both">';
+
+	}
+
+	// End gallery output
+	$output .= "
+		<br style='clear: both;'>
+	</div>\n";
+
+	return $output;
+
+}
+
+// Apply filter to default gallery shortcode
+add_filter( 'post_gallery', 'btsc_shortcode_gallery', 10, 2 );
+
+// Replace with custom shortcode
+function btffsc_shortcode_gallery($attr) {
+    $post = get_post();
+
+    static $instance = 0;
+    $instance++;
+
+    if (!empty($attr['ids'])) {
+        if (empty($attr['orderby'])) {
+            $attr['orderby'] = 'post__in';
+        }
+        $attr['include'] = $attr['ids'];
+    }
+
+    $output = apply_filters('post_gallery', '', $attr);
+
+    if ($output != '') {
+        return $output;
+    }
+
+    if (isset($attr['orderby'])) {
+        $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+        if (!$attr['orderby']) {
+            unset($attr['orderby']);
+        }
+    }
+
+    extract(shortcode_atts(array(
+        'order' => 'ASC',
+        'orderby' => 'menu_order ID',
+        'id' => $post->ID,
+        'itemtag' => '',
+        'icontag' => '',
+        'captiontag' => '',
+        'columns' => 3,
+        'size' => 'thumbnail',
+        'include' => '',
+        'link' => '',
+        'exclude' => ''
+                    ), $attr));
+
+    $id = intval($id);
+
+    if ($order === 'RAND') {
+        $orderby = 'none';
+    }
+
+    if (!empty($include)) {
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    } elseif (!empty($exclude)) {
+        $attachments = get_children(array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+    } else {
+        $attachments = get_children(array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+    }
+
+    if (empty($attachments)) {
+        return '';
+    }
+
+    if (is_feed()) {
+        $output = "\n";
+        foreach ($attachments as $att_id => $attachment) {
+            $output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+        }
+        return $output;
+    }
+
+    //Bootstrap Output Begins Here
+    //Bootstrap needs a unique carousel id to work properly. Because I'm only using one gallery per post and showing them on an archive page, this uses the $post->ID to allow for multiple galleries on the same page.
+
+    $output .= '<h1>oryeba</h1><div id="carousel-' . $post->ID . '" class="carousel slide" data-ride="carousel">'; 
+    $output .= '<!-- Indicators -->';
+    $output .= '<ol class="carousel-indicators">';
+
+    //Automatically generate the correct number of slide indicators and set the first one to have be class="active".
+    $indicatorcount = 0;
+    foreach ($attachments as $id => $attachment) {
+        if ($indicatorcount == 1) {
+            $output .= '<li data-target="#carousel-' . $post->ID . '" data-slide-to="' . $indicatorcount . '" class="active"></li>';
+        } else {
+            $output .= '<li data-target="#carousel-' . $post->ID . '" data-slide-to="' . $indicatorcount . '"></li>';
+        }
+        $indicatorcount++;
+    }
+
+    $output .= '</ol>';
+    $output .= '<!-- Wrapper for slides -->';
+    $output .= '<div class="carousel-inner">';
+    $i = 0;
+
+    //Begin counting slides to set the first one as the active class
+    $slidecount = 1;
+    foreach ($attachments as $id => $attachment) {
+        $link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+        if ($slidecount == 1) {
+            $output .= '<div class="item active">';
+        } else {
+            $output .= '<div class="item">';
+        }
+
+        $image_src_url = wp_get_attachment_image_src($id, $size);
+        $output .= '<img src="' . $image_src_url[0] . '">';
+        $output .= '    </div>';
+
+
+        if (trim($attachment->post_excerpt)) {
+            $output .= '<div class="caption hidden">' . wptexturize($attachment->post_excerpt) . '</div>';
+        }
+
+        $slidecount++;
+    }
+
+    $output .= '</div>';
+    $output .= '<!-- Controls -->';
+    $output .= '<a class="left carousel-control" href="#carousel-' . $post->ID . '" data-slide="prev">';
+    $output .= '<span class="glyphicon glyphicon-chevron-left"></span>';
+    $output .= '</a>';
+    $output .= '<a class="right carousel-control" href="#carousel-' . $post->ID . '" data-slide="next">';
+    $output .= '<span class="glyphicon glyphicon-chevron-right"></span>';
+    $output .= '</a>';
+    $output .= '</div>';
+    $output .= '</dl>';
+    $output .= '</div>';
+
+    return $output;
 }
