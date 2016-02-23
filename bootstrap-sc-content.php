b5351921fe6bdae0720dcdb179d36de6e4c502e6
David Perez <david@closemarketing.es>
2016-02-23 13:04:33 +0100

    Widget contacto

--- bootstrap-sc-content.php
+++ bootstrap-sc-content.php
@@ -40,7 +40,8 @@ require_once( dirname(__FILE__) . '/includes/widgets/button.php'); // Call to Ac
 require_once( dirname(__FILE__) . '/includes/widgets/social-icons.php'); // Recent Posts
 require_once( dirname(__FILE__) . '/includes/widgets/latest-tweets.php'); // Latest Tweets
 require_once( dirname(__FILE__) . '/includes/widgets/recent-posts.php'); // Recent Posts
-require_once( dirname(__FILE__) . '/includes/widgets/childmenu.php'); // Recent Posts
+require_once( dirname(__FILE__) . '/includes/widgets/childmenu.php'); // Child Menu
+require_once( dirname(__FILE__) . '/includes/widgets/contact.php'); // Contact info
 
 //-------------
 require_once( dirname(__FILE__) . '/includes/mce/bsc_shortcodes_tinymce.php'); // Add mce buttons to post editor
--- includes/widgets/contact.php
+++ includes/widgets/contact.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Closemarketing 
- * 
+ * Closemarketing
+ *
  * @package WordPress
  * @subpackage Closemarketing
  * @author Closemarketing <info@closemarketing.es>
@@ -12,207 +12,150 @@
  * http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-if( !class_exists( 'recent_posts' ) ) :
-class recent_posts extends WP_Widget {
-    function recent_posts() {
-    	unregister_widget( 'WP_Widget_Recent_Posts' );
-		
-        $widget_ops = array( 
-            'classname' => 'recent-posts', 
-            'description' => __('The latest posts, with a preview thumb.', 'bsc') 
-        );
+class BSC_contact_ extends WP_Widget {
 
-        $control_ops = array( 'id_base' => 'recent-posts' );
+	function __construct() {
+		$widget_ops = array('classname' => 'widget_contact', 'description' => __('Adds contact info in a widget.', 'bsc'));
+		$control_ops = array('width' => 400, 'height' => 350);
+		parent::__construct('contact_widget', __('Contact Info','bsc'), $widget_ops, $control_ops);
+	}
 
-        WP_Widget::__construct( 'recent-posts', __('Recent Posts', 'bsc'), $widget_ops, $control_ops );
-    }
-    
-    function widget( $args, $instance ) {
-        extract( $args );
+	function widget( $args, $instance ) {
+		extract($args);
+		$contact_company = apply_filters( 'widget_contact_company', empty( $instance['contact_company'] ) ? '' : $instance['contact_company'], $instance );
+		$contact_dir1 = apply_filters( 'widget_contact_dir1', empty( $instance['contact_dir1'] ) ? '' : $instance['contact_dir1'], $instance );
+		$contact_dir2 = apply_filters( 'widget_contact_dir2', empty( $instance['contact_dir2'] ) ? '' : $instance['contact_dir2'], $instance );
+		$contact_zip = apply_filters( 'widget_contact_zip', empty( $instance['contact_zip'] ) ? '' : $instance['contact_zip'], $instance );
+		$contact_city = apply_filters( 'widget_contact_city', empty( $instance['contact_city'] ) ? '' : $instance['contact_city'], $instance );
+		$contact_state = apply_filters( 'widget_contact_state', empty( $instance['contact_state'] ) ? '' : $instance['contact_state'], $instance );
+		$contact_phone = apply_filters( 'widget_contact_phone', empty( $instance['contact_phone'] ) ? '' : $instance['contact_phone'], $instance );
+		$contact_email = apply_filters( 'widget_contact_email', empty( $instance['contact_email'] ) ? '' : $instance['contact_email'], $instance );
+		$contact_desc = apply_filters( 'widget_contact_desc', empty( $instance['contact_desc'] ) ? '' : $instance['contact_desc'], $instance );
 
-        /* User-selected settings. */
-        if( !isset( $instance['title'] ) )
-            $instance['title'] = '';
-            
-        $title = apply_filters('widget_title', $instance['title'] );
+		echo $before_widget; ?>
 
-        $items = isset( $instance['items']) ? $instance['items'] : '';
-        $show = isset( $instance['show'] ) ? $instance['show'] : 'nothing';
-        $excerpt = isset( $instance['excerpt']) ? $instance['excerpt'] : 'no';
-        $excerpt_length = isset( $instance['excerpt_length'] ) ? $instance['excerpt_length'] : 10;
-        $show_comments = isset( $instance ['show_comments'] ) ? $instance['show_comments'] : 'no';
-        $show_date = isset( $instance ['show_date'] ) ? $instance['show_date'] : 'no';
+			<div class="companyinfo" itemscope itemtype="http://schema.org/LocalBusiness">
+			  <p><strong><span itemprop="name"><?php echo $contact_company; ?></span></strong></p>
 
-        echo $before_widget;
-        
-        if ( $title ) echo $before_title . $title . $after_title;
+			  <?php if($contact_desc) { ?>
+				  <span itemprop="description"><?php echo $contact_desc; ?></span>
+			  <?php } ?>
+			  <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+				<i class="fa fa-map-marker"></i>
+			    <span itemprop="streetAddress">
+					<?php echo $contact_dir1; ?></br><?php echo $contact_dir2; ?>
+				</span>
+				<br/>
+				<span itemprop="postalCode"><?php echo $contact_zip; ?> </span>
+			    <span itemprop="addressLocality"><?php echo $contact_city; ?></span>
+			    <span itemprop="addressRegion">(<?php echo $contact_state; ?>)</span>
+			  </div>
+			  <?php if($contact_phone) { ?>
+				  <i class="fa fa-phone-square"></i>
+				  <?php _e('Phone','bsc');?>: <span itemprop="telephone"><?php echo $contact_phone; ?></span><br/>
+			  <?php } ?>
+			  <?php if($contact_email) { ?>
+			  <i class="fa fa-envelope-o"></i>
+			  <?php _e('E-mail','bsc');?>:
+			  <a href="mailto:<?php echo $contact_email; ?>">
+			  	<span itemprop="email"><?php echo $contact_email; ?></span>
+			  </a>
+		  	  <?php } ?>
+			</div>
 
-        $args = array(
-           'posts_per_page' => $items,
-           'orderby' => 'date',
-           'ignore_sticky_posts' => 1
-        );                            
-        
-        $args['order'] = 'DESC'; 
-        
-        $myposts = new WP_Query( $args );
-    	
-        $html = "\n";       
-        $html .= '<div class="recent-post group">'."\n";
-        
-        if( $myposts->have_posts() ) : while( $myposts->have_posts() ) { $myposts->the_post();
-            
-            $img = '';
-            if(has_post_thumbnail())
-                { $img = get_the_post_thumbnail( get_the_ID(), 'thumbnail' ); }
-    		    
-            $html .= '<div class="hentry-post group row">'."\n";	
-				
-            if ( $show == 'thumb' ) {
-                $html .= '<div class="thumb-img col-xs-4">';
-                $html .= '<a href="'.get_permalink().'" title="'.get_the_title().'" class="title">';
-                $html .= $img . '</a></div>';
-                $html .= '<div class="col-xs-8 text">';
-            } elseif ( $show == 'date' ) {
-                $html .= '<div class="thumb-date"><span class="month">' . get_the_date('M') . '</span><span class="day">' . get_the_date('d') . '</span></div>';
-                $html .= '<div class="col-xs-8 text">';
-            } else {
-                $html .= '<div class="col-xs-8 text without-thumbnail">';
-            }
-            
-            $html .= the_title( '<a href="'.get_permalink().'" title="'.get_the_title().'" class="title">', '</a>', false );
-            
-            if ( $excerpt == 'yes' ) {
-                $html .= get_the_excerpt();
-            }
-            if ( $show_date == 'yes' ) {
-                $html .= '<p class="post-date">';
-                $html .= '<span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> ';
-                $html .= '<time datetime="'.get_the_time(__('F jS, Y','bsc')).'" pubdate>';
-                $html .= get_the_time(__('F jS, Y','bsc'));
-                $html .= '</time></p>';
-            }
+		<?php
+		echo $after_widget;
+	}
 
-            if ( $show_comments == 'yes' ) {
-                $html .= '<p class="post-comments">';
-                $number_comments = get_comments_number();
-                if ( $number_comments == 0 ) {
-                    $html .= __('0 comments', 'bsc');
-                } elseif ( $number_comments == 1 ) {
-                    $html .= __('1 comment', 'bsc');
-                } else {
-                    $html .= $number_comments .' '. __('comments', 'bsc');
-                }
-                $html .= '</p>';
-            }
-			
-            $html .= '</div>'."\n";
-    		$html .= '</div>'."\n".'<hr/>';
-        
-        } endif;
-        
-        wp_reset_query();
-        $html .= '</div>';
-        
-        echo $html;
-        
-        add_filter( 'the_content_more_link', 'yit_sc_more_link', 10, 3 );  //shortcode in more links
-        
-        echo $after_widget;
-    }
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
 
-    function update( $new_instance, $old_instance ) {
-        $instance = $old_instance;
+        $instance['contact_company'] =  $new_instance['contact_company'];
+        $instance['contact_dir1'] =  $new_instance['contact_dir1'];
+        $instance['contact_dir2'] =  $new_instance['contact_dir2'];
+        $instance['contact_zip'] =  $new_instance['contact_zip'];
+        $instance['contact_city'] =  $new_instance['contact_city'];
+        $instance['contact_state'] =  $new_instance['contact_state'];
+        $instance['contact_phone'] =  $new_instance['contact_phone'];
+        $instance['contact_email'] =  $new_instance['contact_email'];
+        $instance['contact_desc'] =  $new_instance['contact_desc'];
 
-        $instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['filter'] = isset($new_instance['filter']);
+		return $instance;
+	}
 
-        $instance['items'] = $new_instance['items'];
-
-        $instance['show'] = $new_instance['show'];
-
-        $instance['excerpt'] = $new_instance['excerpt'];
-
-        $instance['excerpt_length'] = $new_instance['excerpt_length'];
-        
-        $instance['show_date'] = $new_instance['show_date'];
-        
-        $instance['show_comments'] = $new_instance['show_comments'];
-
-        return $instance;
-    }
-
-    function form( $instance ) {   
-        /* Impostazioni di default del widget */
-        $defaults = array( 
-            'title' => __('Recent Posts', 'bsc'), 
-            'items' => 3,
-            'show' => 'thumb',
-            'excerpt' => 'no',
-            'excerpt_length' => '10',
-            'more_text' => '|| ' . __( 'Read More', 'bsc' ),
-            'show_comments' => 'no'
-        );
-        
-        $instance = wp_parse_args( (array) $instance, $defaults ); ?>
-        
-        <p>
-            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title', 'bsc' ) ?>:
-                 <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" class="widefat" />
-            </label>
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance,
+                        array( 'contact_company' => '',
+								'contact_dir1' => '',
+								'contact_dir2' => '',
+								'contact_zip' => '',
+								'contact_city' => '',
+								'contact_state' => '',
+								'contact_phone' => '',
+								'contact_email' => '',
+								'contact_desc' => '',
+							) );
+		$contact_company = esc_textarea($instance['contact_company']);
+		$contact_dir1 = esc_textarea($instance['contact_dir1']);
+		$contact_dir2 = esc_textarea($instance['contact_dir2']);
+		$contact_zip = esc_textarea($instance['contact_zip']);
+		$contact_city = esc_textarea($instance['contact_city']);
+		$contact_state = esc_textarea($instance['contact_state']);
+		$contact_phone = esc_textarea($instance['contact_phone']);
+		$contact_email = esc_textarea($instance['contact_email']);
+		$contact_desc = esc_textarea($instance['contact_desc']);
+?>
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_company'); ?>"><?php _e('Contact Name','bsc'); ?>:</label>
+			<input class="widefat" id="<?php echo $this->get_field_id('contact_company'); ?>" name="<?php echo $this->get_field_name('contact_company'); ?>" type="text" value="<?php echo esc_attr($contact_company); ?>" />
         </p>
 
-        <p>
-            <label for="<?php echo $this->get_field_id( 'items' ); ?>"><?php _e( 'Items', 'bsc' ) ?>:
-                <input type="text" id="<?php echo $this->get_field_id( 'items' ); ?>" name="<?php echo $this->get_field_name( 'items' ); ?>" value="<?php echo $instance['items']; ?>" size="3" />
-            </label>
-        </p>
-        
-        <p>
-            <label for="<?php echo $this->get_field_id( 'show' ); ?>"><?php _e( 'Show', 'bsc' ) ?>:
-                 <select id="<?php echo $this->get_field_id( 'show' ); ?>" name="<?php echo $this->get_field_name( 'show' ); ?>">
-                    <option value="nothing" <?php selected( $instance['show'], 'nothing' ) ?>><?php _e( 'Nothing', 'bsc' ) ?></option>
-                     <option value="thumb" <?php selected( $instance['show'], 'thumb' ) ?>><?php _e( 'Thumbnails', 'bsc' ) ?></option>
-                     <option value="date" <?php selected( $instance['show'], 'date' ) ?>><?php _e( 'Date', 'bsc' ) ?></option>
-                 </select>
-            </label>
-        </p>
-        
-        <p>
-            <label for="<?php echo $this->get_field_id( 'excerpt' ); ?>"><?php _e( 'Show Excerpt', 'bsc' ) ?>:
-                 <select id="<?php echo $this->get_field_id( 'excerpt' ); ?>" name="<?php echo $this->get_field_name( 'excerpt' ); ?>">
-                    <option value="yes" <?php selected( $instance['excerpt'], 'yes' ) ?>><?php _e( 'Yes', 'bsc' ) ?></option>
-                    <option value="no" <?php selected( $instance['excerpt'], 'no' ) ?>><?php _e( 'No', 'bsc' ) ?></option>
-                 </select>
-            </label>
-        </p>
-        
-        <p>
-            <label for="<?php echo $this->get_field_id( 'excerpt_length' ); ?>"><?php _e( 'Excerpt Lenght', 'bsc' ) ?>:
-                 <input type="text" id="<?php echo $this->get_field_id( 'excerpt_length' ); ?>" name="<?php echo $this->get_field_name( 'excerpt_length' ); ?>" value="<?php echo $instance['excerpt_length']; ?>"  size="3" />
-            </label>
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_dir1'); ?>"><?php _e('Address info 1','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_dir1'); ?>" name="<?php echo $this->get_field_name('contact_dir1'); ?>" type="text" value="<?php echo esc_attr($contact_dir1); ?>" />
         </p>
 
-        <p>
-            <label for="<?php echo $this->get_field_id( 'show_date' ); ?>"><?php _e( 'Show Date', 'bsc' ) ?>:
-                <select id="<?php echo $this->get_field_id( 'show_date' ); ?>" name="<?php echo $this->get_field_name( 'show_date' ); ?>">
-                    <option value="yes" <?php selected( $instance['show_date'], 'yes' ) ?>><?php _e( 'Yes', 'bsc' ) ?></option>
-                    <option value="no" <?php selected( $instance['show_date'], 'no' ) ?>><?php _e( 'No', 'bsc' ) ?></option>
-                </select>
-            </label>
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_dir2'); ?>"><?php _e('Address info 2','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_dir2'); ?>" name="<?php echo $this->get_field_name('contact_dir2'); ?>" type="text" value="<?php echo esc_attr($contact_dir2); ?>" />
         </p>
 
-        <p>
-            <label for="<?php echo $this->get_field_id( 'show_comments' ); ?>"><?php _e( 'Show Comments', 'bsc' ) ?>:
-                <select id="<?php echo $this->get_field_id( 'show_comments' ); ?>" name="<?php echo $this->get_field_name( 'show_comments' ); ?>">
-                    <option value="yes" <?php selected( $instance['show_comments'], 'yes' ) ?>><?php _e( 'Yes', 'bsc' ) ?></option>
-                    <option value="no" <?php selected( $instance['show_comments'], 'no' ) ?>><?php _e( 'No', 'bsc' ) ?></option>
-                </select>
-            </label>
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_zip'); ?>"><?php _e('ZIP','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_zip'); ?>" name="<?php echo $this->get_field_name('contact_zip'); ?>" type="text" value="<?php echo esc_attr($contact_zip); ?>" />
         </p>
-    <?php
-    }
-}
+
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_city'); ?>"><?php _e('City','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_city'); ?>" name="<?php echo $this->get_field_name('contact_city'); ?>" type="text" value="<?php echo esc_attr($contact_city); ?>" />
+        </p>
+
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_state'); ?>"><?php _e('State','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_state'); ?>" name="<?php echo $this->get_field_name('contact_state'); ?>" type="text" value="<?php echo esc_attr($contact_state); ?>" />
+        </p>
+
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_phone'); ?>"><?php _e('Phone','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_phone'); ?>" name="<?php echo $this->get_field_name('contact_phone'); ?>" type="text" value="<?php echo esc_attr($contact_phone); ?>" />
+        </p>
+
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_email'); ?>"><?php _e('Email','bsc'); ?>:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('contact_email'); ?>" name="<?php echo $this->get_field_name('contact_email'); ?>" type="text" value="<?php echo esc_attr($contact_email); ?>" />
+        </p>
+
+		<p>
+            <label for="<?php echo $this->get_field_id('contact_desc'); ?>"><?php _e('Contact Description','bsc'); ?>:</label>
+		<textarea class="widefat" rows="5" cols="20" id="<?php echo $this->get_field_id('contact_desc'); ?>" name="<?php echo $this->get_field_name('contact_desc'); ?>"><?php echo $contact_desc; ?></textarea>
+        </p>
+
+		<?php
+		}
+		}
+
+
 /** Register sidebars by running twentyten_widgets_init() on the widgets_init hook. */
 
-add_action( 'widgets_init', create_function( '', 'register_widget( "recent_posts" );' ) );
-endif;
+add_action( 'widgets_init', create_function( '', 'register_widget( "BSC_contact_" );' ) );
